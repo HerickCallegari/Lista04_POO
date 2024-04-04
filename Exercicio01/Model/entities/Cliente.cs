2424fa5da8512084461486d3491c0f0078883d4e
Herick Campos <herickzeno@gmail.com>
2024-04-04 08:49:22 -0300

    Cliente pronto

--- Exercicio01/Model/entities/Cliente.cs
+++ Exercicio01/Model/entities/Cliente.cs
@@ -8,6 +8,30 @@ namespace Exercicio01.Model.entities
     {
     internal class Cliente
         {
+        public string NOME { get; set; }
+        public string EMAIL { get; set; }
+        public string ENDERECO { get; set; }
+        public string TELEFONE { get; set; }
+
+        public Cliente ( ) { }
+
+        public Cliente ( string nome, string email, string endereco, string telefone ) : this()
+            {
+            NOME = nome;
+            EMAIL = email;
+            ENDERECO = endereco;
+            TELEFONE = telefone;
+            }
+
+        public void AtualizarEndereco( string endereco )
+            {
+            ENDERECO = endereco;
+            }
+
+        public void AtualizarTelefone( string telefone )
+            {
+            TELEFONE = telefone;
+            }
 
         }
     }
